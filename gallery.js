023db0473df57b638a23b8ad5dea1a1f7bbb8139
Dustin Krasnowski <krasman3789@gmail.com>
2024-09-14 21:38:54 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,25 +1,41 @@
+// Array of image data (can easily be extended without hard-coding new values)
+const galleryImages = [
+    { thumbnail: "images/fullsize1.png", fullsize: "images/fullsize1.png", alt: "Thumbnail 1" },
+    { thumbnail: "images/fullsize2.png", fullsize: "images/fullsize2.png", alt: "Thumbnail 2" },
+    { thumbnail: "images/fullsize3.png", fullsize: "images/fullsize3.png", alt: "Thumbnail 3" }
+];
+
 // Preload gallery images
 function preloadImages() {
-    const images = [
-        "fullsize1.png",
-        "fullsize2.png",
-        "fullsize3.png"
-    ];
-    images.forEach((src) => {
+    galleryImages.forEach(image => {
         const img = new Image();
-        img.src = src;
+        img.src = image.fullsize;
     });
 }
 
-// Show full-size image when a thumbnail is clicked
-function showFullImage(thumbnail) {
-    const fullSizeImage = document.getElementById('fullSizeImage');
-
-    // Set the source of the full-size image to the data-fullsize attribute of the clicked thumbnail
-    fullSizeImage.src = thumbnail.dataset.fullsize;
+// Insert gallery images dynamically
+function insertGalleryImages() {
+    const galleryContainer = document.querySelector('.gallery-container');
+    galleryImages.forEach(image => {
+        const imgElement = document.createElement('img');
+        imgElement.className = 'thumbnail';
+        imgElement.src = image.thumbnail;
+        imgElement.alt = image.alt;
+        imgElement.dataset.fullsize = image.fullsize;
+        imgElement.onclick = () => showFullImage(imgElement); // Assign click event handler
+        galleryContainer.appendChild(imgElement);
+    });
 }
 
-// Run preload and rollover setup on page load
+// Show full-size image
+function showFullImage(thumbnail) {
+    const fullSizeImage = document.getElementById('fullSizeImage');
+    fullSizeImage.src = thumbnail.dataset.fullsize;
+    fullSizeImage.alt = thumbnail.alt; // Set the alt text
+}
+
+// Run preload and setup on page load
 function onPageLoad() {
     preloadImages();
+    insertGalleryImages();
 }
