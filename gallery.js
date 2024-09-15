b01bcb83d9da358de69a2ee8dc8dfef580bdfdc9
Dustin Krasnowski <krasman3789@gmail.com>
2024-09-14 20:27:10 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,33 +1,26 @@
 // Preload gallery images
 function preloadImages() {
-    const images = [
-        "fullsize1.png",
-        "fullsize2.png",
-        "fullsize3.png",
-        "thumbnail1.png",
-        "thumbnail2.png",
-        "thumbnail3.png"
-    ];
-    images.forEach((src) => {
-        const img = new Image();
-        img.src = src;
+    const thumbnails = document.querySelectorAll(".thumbnail"); // Select all thumbnails
+
+    thumbnails.forEach((thumbnail) => {
+        const fullSizeSrc = thumbnail.getAttribute("data-fullsize");
+        const fullSizeImage = new Image();
+        fullSizeImage.src = fullSizeSrc; // Preload the full-size image
     });
 }
 
-// Rollover function for gallery images
+// Dynamic Rollover Function for Gallery Images
 function setupRollovers() {
-    const image1 = document.getElementById("image1");
-    const image2 = document.getElementById("image2");
-    const image3 = document.getElementById("image3");
+    const thumbnails = document.querySelectorAll(".thumbnail"); // Select all thumbnails
 
-    image1.onmouseover = () => image1.src = "fullsize1.png";
-    image1.onmouseout = () => image1.src = "thumbnail1.png";
+    thumbnails.forEach((thumbnail) => {
+        const fullSizeSrc = thumbnail.getAttribute("data-fullsize");
+        const thumbnailSrc = thumbnail.src; // Get the original thumbnail source
 
-    image2.onmouseover = () => image2.src = "fullsize2.png";
-    image2.onmouseout = () => image2.src = "thumbnail2.png";
-
-    image3.onmouseover = () => image3.src = "fullsize3.png";
-    image3.onmouseout = () => image3.src = "thumbnail3.png";
+        // Handle mouseover and mouseout for rollover effect
+        thumbnail.onmouseover = () => thumbnail.src = fullSizeSrc;
+        thumbnail.onmouseout = () => thumbnail.src = thumbnailSrc;
+    });
 }
 
 // Run preload and rollover setup on page load
