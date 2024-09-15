e4a157b95a6b2ae5f063007d55b3c821ea5c0ffe
Dustin Krasnowski <krasman3789@gmail.com>
2024-09-14 18:57:20 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,6 +1,13 @@
 // Preload gallery images
 function preloadImages() {
-    const images = ["images/fullsize1.jpg", "images/fullsize2.jpg", "images/fullsize3.jpg"];
+    const images = [
+        "images/fullsize1.jpg",
+        "images/fullsize2.jpg",
+        "images/fullsize3.jpg",
+        "images/thumbnail1.jpg",
+        "images/thumbnail2.jpg",
+        "images/thumbnail3.jpg"
+    ];
     images.forEach((src) => {
         const img = new Image();
         img.src = src;
@@ -14,13 +21,13 @@ function setupRollovers() {
     const image3 = document.getElementById("image3");
 
     image1.onmouseover = () => image1.src = "images/fullsize1.jpg";
-    image1.onmouseout = () => image1.src = "images/thumbnail1.jpg";
+    image1.onmouseout = () => image1.src = "images/fullsize1.jpg";
 
     image2.onmouseover = () => image2.src = "images/fullsize2.jpg";
-    image2.onmouseout = () => image2.src = "images/thumbnail2.jpg";
+    image2.onmouseout = () => image2.src = "images/fullsize2.jpg";
 
     image3.onmouseover = () => image3.src = "images/fullsize3.jpg";
-    image3.onmouseout = () => image3.src = "images/thumbnail3.jpg";
+    image3.onmouseout = () => image3.src = "images/fullsize3.jpg";
 }
 
 // Run preload and rollover setup on page load
